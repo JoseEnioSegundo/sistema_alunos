7e0ca151f6ce37fd128a3b7790a6e024a924f7e7
josesegundo <jose.ardinoseg@gmail.com>
2026-03-03 19:34:58 -0300

    feat: :lipstick: Adicionando matricula no forms

--- alunos/forms.py
+++ alunos/forms.py
@@ -4,9 +4,11 @@ from .models import Aluno
 class AlunoForm(forms.ModelForm):
     class Meta:
         model = Aluno
-        fields = ['nome', 'curso', 'data_nascimento']
+        fields = ['nome', 'email', 'matricula', 'curso', 'data_nascimento']
         widgets = {
             'nome': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'matricula': forms.TextInput(attrs={'class': 'form-control'}),
             'curso': forms.TextInput(attrs={'class': 'form-control'}),
             'data_nascimento': forms.DateInput(attrs={
                 'class': 'form-control',
