f0cc0829340f264ce8a5c8f8b0f862fa9c6d34cd
josesegundo <jose.ardinoseg@gmail.com>
2026-03-03 18:11:29 -0300

    feat: :white_check_mark: Adicionando o cadastro de alunos na View

--- alunos/views.py
+++ alunos/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from .models import Aluno
+from django.shortcuts import redirect
+from .forms import AlunoForm
 
 def lista_alunos(request):
     alunos = Aluno.objects.all()
     return render(request, 'alunos/lista_alunos.html', {'alunos': alunos})
+
+
+def cadastrar_aluno(request):
+    if request.method == 'POST':
+        form = AlunoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_alunos')
+    else:
+        form = AlunoForm()
+
+    return render(request, 'alunos/cadastrar_aluno.html', {'form': form})
